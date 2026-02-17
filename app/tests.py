eb71c40383b30a41d28bfcbee8843cba8d99d3b4
Tangudu Abhiram <tanguduabhiram@Tangudus-MacBook-Air.local>
2026-02-17 12:26:58 +0530

    tests

--- app/tests.py
+++ app/tests.py
@@ -3,4 +3,4 @@ from django.test import TestCase
 # Create your tests here.
 class MyTestCase(TestCase):
     def test_example(self):
-        self.assertEqual(1, 1)
+        self.assertEqual(2+2, 4)
